5e804d79ec89b0356f2a78afaca42659d291b64a
ardi0303 <ardi210303@gmail.com>
2025-08-25 09:32:20 +0700

    fix: adjust padding for CV button on About page for better responsiveness

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -89,7 +89,7 @@ export default function About() {
                 );
               })}
             </div>
-            <button className="font-jakarta-sans dark-button rounded-xl px-6 py-2 text-xs font-semibold md:text-base">
+            <button className="font-jakarta-sans dark-button rounded-xl px-6 py-2 text-xs font-semibold md:px-8 md:py-2 md:text-base">
               <a
                 href="https://drive.google.com/file/d/1VegzE_5euBCLiUuTxFYxKqt9F17TS0Cn/"
                 target="_blank"
